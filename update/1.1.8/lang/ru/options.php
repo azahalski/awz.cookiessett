40438fc7430de1a1031a6e0de523703214715db4
Andrew Zahalski <andrew@zahalski.dev>
2025-06-09 09:59:42 +0300

    gulp checkpoint

---
+++ update/1.1.8/lang/ru/options.php
@@ -0,0 +1,16 @@
+<?php
+$MESS["AWZ_COOKIESSETT_OPT_TITLE"] = "Настройки модуля AWZ: Согласие о cookies";
+$MESS["AWZ_COOKIESSETT_OPT_SECT1"] = "Настройки модуля";
+$MESS["AWZ_COOKIESSETT_OPT_SECT2"] = "Права доступа";
+$MESS["AWZ_COOKIESSETT_OPT_SHOW_DESC"] = "Вы можете самостоятельно разместить компонент перед закрывающим тегом BODY вашего шаблона (<b>Рекомендуется!</b> - <a target=\"_blank\" href=\"https://zahalski.dev/modules/awz.cookiessett/docs/\">для разработчиков</a>).<br>Или отметить публикацию компонента на обработчике (опция ниже).";
+$MESS["AWZ_COOKIESSETT_OPT_SHOW"] = "Публиковать компонент (обработчик onEndBufferContent)";
+$MESS["AWZ_COOKIESSETT_OPT_L_BTN_SAVE"] = "Сохранить настройки";
+$MESS["AWZ_COOKIESSETT_OPT_PRM_BUTTON_OK"] = "Текст на кнопке согласия";
+$MESS["AWZ_COOKIESSETT_OPT_THEME"] = "Цветовая схема";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_DEFAULT"] = "По умолчанию";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_RED"] = "Красная";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_BLUE"] = "Голубая";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_GREEN"] = "Зеленая";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_YELOW"] = "Желтая";
+$MESS["AWZ_COOKIESSETT_OPT_THEME_PINK"] = "Розово-красный";
+$MESS["AWZ_COOKIESSETT_OPT_SITE_ID"] = "Выберите сайт";
