7edd1b25c77dfdcdd540d9496625b3a9e8c14260
Andrew Zahalski <andrew@zahalski.dev>
2025-07-01 00:18:34 +0300

    gulp checkpoint

---
+++ update/1.2.2/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.2.2",
+	"VERSION_DATE" => "2025-07-01 00:02:00"
+);
+?>
---
+++ update/1.2.2/lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?
+$MESS['AWZ_COOKIESSETT_INSTALL_TITLE'] = "Удаление модуля AWZ: Согласие о cookies";
+$MESS['AWZ_COOKIESSETT_MODULE_NAME'] = "AWZ: Согласие о cookies";
+$MESS['AWZ_COOKIESSETT_MODULE_DESCRIPTION'] = "Модуль для работы с согласием пользователя на использование cookies";
+$MESS['AWZ_PARTNER_NAME'] = "Andrew Zahalski";
+$MESS['MOD_UNINST_SAVE_OPTS'] = "Вы можете сохранить основные настройки модуля";
+$MESS['MOD_UNINST_SAVE_EVENTS_DESC'] = "Вы можете сохранить почтовые шаблоны";
+$MESS['MOD_UNINST_SAVE_OPTS_LABEL'] = "Сохранить настройки";
